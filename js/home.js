b792d55caa32b4c3d613a066a8cc342ecdc0c20d
NeshGogo <rafaelaguero10@hotmail.com>
2020-08-06 10:59:11 -0400

    Manejo de likes

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 import { Comment } from '../models/comment.js'
 import { auth, firestore } from '../services/firebase.js';
+import firebase from '../services/firebase.js';
 
 const USERS = 'users';
 const POSTS = 'posts';
@@ -41,7 +42,7 @@ firestore.collection(POSTS).orderBy('date', 'desc').get().then(async (snapshots)
         </div>
         <img class="post-card--img" src="${post.imageUrl}" alt="Imagen del post">
         <div class="post-card--footer-img">
-          <a><img id="postLikeIcon-${post.id}" alt="icono de favorito"></a>
+          <a id="addPostLike-${post.id}"><img id="postLikeIcon-${post.id}" alt="icono de favorito"></a>
           <a><img src="./assets/img/icon-comment.png" alt=""></a>
           <p><span id="postLikes-${post.id}">${post.likes}</span> Me gusta</p>
         </div>
@@ -63,11 +64,12 @@ firestore.collection(POSTS).orderBy('date', 'desc').get().then(async (snapshots)
         </div>
       </div>
     `;
-    const comments =  getPostCommets(post.id);
+    const comments = getPostCommets(post.id);
     const user = await getuserById(post.userRef);
     document.querySelector(`#postCartHeaderUserImg-${post.id}`).src = user.imageUrl;
     document.querySelector(`#postCartHeaderUserName-${post.id}`).innerText = user.userName;
     document.querySelector(`#postCartDescriptionUserName-${post.id}`).innerText = user.userName;
+    document.querySelector(`#addPostLike-${post.id}`).onclick = () => addPostLike(post.id);
     document.querySelector(`#addPostComment-${post.id}`).onclick = () => {
       const inputComment = document.querySelector(`#inputComment-${post.id}`);
       addPostComment(post.id, inputComment.value);
@@ -111,21 +113,31 @@ const addPostComment = async (postId, value) => {
     }
   }
 }
-
+const addPostLike = async (postId) => {
+  const docRef = firestore.collection(POSTS).doc(postId);
+  const post = (await docRef.get()).data();
+  const liked = !post.likesRef.includes(currentUserId);
+  docRef.update({
+    likes: liked? post.likes + 1 : post.likes - 1,
+    likesRef: liked?
+      firebase.firestore.FieldValue.arrayUnion(currentUserId)
+      : firebase.firestore.FieldValue.arrayRemove(currentUserId),
+  })
+}
 const onPostChange = (postId) => {
   firestore.collection(POSTS).doc(postId)
-  .onSnapshot(snapShot => {
-    const postComment = document.querySelector(`#postCommentAmout-${postId}`);
-    const postLikes = document.querySelector(`#postLikes-${postId}`);
-    const postLikeIcon = document.querySelector(`#postLikeIcon-${postId}`)
-    const post = snapShot.data();
-    const likeIcon = post.likesRef.includes(currentUserId) ?
-      './assets/img/icon-heart-red.png'
-      : './assets/img/icon-heart-outline.png';
-    postLikes.innerText =  post.likes;
-    postComment.innerText = post.commentsRef.length;
-    postLikeIcon.src = likeIcon;
-  });
+    .onSnapshot(snapShot => {
+      const postComment = document.querySelector(`#postCommentAmout-${postId}`);
+      const postLikes = document.querySelector(`#postLikes-${postId}`);
+      const postLikeIcon = document.querySelector(`#postLikeIcon-${postId}`)
+      const post = snapShot.data();
+      const likeIcon = post.likesRef.includes(currentUserId) ?
+        './assets/img/icon-heart-red.png'
+        : './assets/img/icon-heart-outline.png';
+      postLikes.innerText = post.likes;
+      postComment.innerText = post.commentsRef.length;
+      postLikeIcon.src = likeIcon;
+    });
 }
 
 document.querySelector('#signOut').onclick = () => {
